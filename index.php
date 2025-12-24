73ecd3e2b8f85b0fe2975691175a7684b9bef50f
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-12-24 18:00:44 +0100

    feat: set access control headers

--- index.php
+++ index.php
@@ -2,3 +2,7 @@
 
 require_once('vendor/autoload.php');
 require_once('config/config.php');
+
+header('Access-Control-Allow-Origin: ' . $config["application"]["allowed_origin"]);
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
