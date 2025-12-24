043b4cc6a9243b5f1fe06c117a9395058d2d05f1
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-12-24 18:01:25 +0100

    feat: add router

--- index.php
+++ index.php
@@ -6,3 +6,5 @@ require_once('config/config.php');
 header('Access-Control-Allow-Origin: ' . $config["application"]["allowed_origin"]);
 header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
+
+require_once('router.php');
